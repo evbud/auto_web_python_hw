da347d5078378c00c655b0b8a363240897669129
Evgenii B <budich@me.com>
2023-09-06 03:21:41 +0300

    Corrected hw_04 Project

--- hw_04/test_rest_api.py
+++ hw_04/test_rest_api.py
@@ -12,20 +12,22 @@ with open('testdata.yaml', encoding='utf-8') as f:
     title = testdata['test_title']
     description = testdata['test_description']
     content = testdata['test_content']
+    my_post_title = testdata['my_post_title']
+    not_my_post_title = testdata['not_my_post_title']
 
 
 def test_rest_my_title():
     logging.info('Test1 Rest starting')
     rest_session = BaseRest(address, username, password)
     posts = rest_session.get_own_posts(address_posts)
-    assert 'kitty' in posts, 'Test1 Rest failed'
+    assert my_post_title in posts, 'Test1 Rest failed'
 
 
-def test_rest_get_post():
+def test_rest_not_my_title():
     logging.info('Test2 Rest starting')
     rest_session = BaseRest(address, username, password)
     posts = rest_session.get_other_posts(address_posts)
-    assert 'Test post' in posts, 'Test2 Rest failed'
+    assert not_my_post_title in posts, 'Test2 Rest failed'
 
 
 def test_rest_new_post():
